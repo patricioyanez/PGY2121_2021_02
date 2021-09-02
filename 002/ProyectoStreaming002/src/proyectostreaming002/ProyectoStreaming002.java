cae8baf85480009680a8f714889656808472e7e1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-09-02 14:41:12 -0400

    2da

--- 002/ProyectoStreaming002/src/proyectostreaming002/ProyectoStreaming002.java
+++ 002/ProyectoStreaming002/src/proyectostreaming002/ProyectoStreaming002.java
@@ -6,7 +6,20 @@ public class ProyectoStreaming002 {
 
 
     public static void main(String[] args) {
-
+        System.out.println("****  Inicio de la aplicación  ****");
+        
+        Cancion cancion01 = new Cancion();
+        cancion01.imprimir();
+        
+        cancion01.setTitulo("Miño");
+        cancion01.setArtista("Los Bunkers");
+        cancion01.setSegundos(360);
+        
+        cancion01.imprimir();
+        
+        
+        
+        
     }
     
 }
