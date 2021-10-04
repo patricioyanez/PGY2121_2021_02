d50620c175a2afe5e91d88dcf39db9d4d02267ea
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-10-04 14:30:56 -0300

    Empresa / Empleados

--- 002/Experiencia 3 y 4/Scootin002/src/scootin002/Empresa.java
+++ 002/Experiencia 3 y 4/Scootin002/src/scootin002/Empresa.java
@@ -15,8 +15,41 @@ public class Empresa {
     // crear un método que permita agregar a 1 empleado
     public boolean agregar(Empleado empleado)
     {
+        // Evitar que se repita el rut
+        if(this.buscar(empleado.getRut()))
+        {
+            System.out.println("El rut " + empleado.getRut() +" ya existe");
+            return false;
+        }
         return empleados.add(empleado);
     }
     // crear otro método que permita saber si existe un empleado X
+    // se debe buscar por rut
+    public boolean buscar(String rut)
+    {
+        for(Empleado empleado : empleados)
+        {
+            if(rut.equalsIgnoreCase(empleado.getRut()))
+                return true;            
+        }
+        return false;
+    }
+    
+    public void listar()
+    {
+        for(Empleado empleado: empleados)
+        {
+            System.out.println(empleado);
+        }
+    }
+    public boolean eliminar(String rut)
+    {
+        for(Empleado empleado : empleados)
+        {
+            if(rut.equalsIgnoreCase(empleado.getRut()))
+                return empleados.remove(empleado);            
+        }
+        return false;
+    }
     
 }
--- 002/Experiencia 3 y 4/Scootin002/src/scootin002/Scootin002.java
+++ 002/Experiencia 3 y 4/Scootin002/src/scootin002/Scootin002.java
@@ -13,9 +13,29 @@ public class Scootin002 {
 
     /**
      * @param args the command line arguments
+     * Esta reemplaza a TEST EMPRESA!!!!!
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Puesto puesto1 = new Puesto(100, "Vendedor");
+        Puesto puesto2 = new Puesto(200, "Supervisor");
+        
+        Empleado empleado1 = new Empleado("1-9", "Juan Diaz", "Masculino", 1, 30, puesto1);
+        Empleado empleado2 = new Empleado("2-7", "Juana Aros", "Femenino", 5, 40, puesto2);
+        
+        Empresa scootin = new Empresa();
+        
+        // agregar empleados a la empresa
+        scootin.agregar(empleado1);
+        scootin.agregar(empleado2);
+        
+        // detecta que ya existe el empleado
+        scootin.agregar(empleado2);
+        
+        // lista los empleados
+        scootin.listar();
+        
+        // buscar si un rut existe y eliminarlo
     }
     
 }
